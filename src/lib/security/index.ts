b6d8841c7d67b076e12e48416ec79240c230586c
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-08-08 23:23:00 +0200

    feat(security): export security modules and interfaces

---
+++ src/lib/security/index.ts
@@ -0,0 +1,3 @@
+export { useRBAC, type Role, type Permission, type User, type RBACState } from './rbac';
+export { EncryptionService, encryptionService } from './encryption';
+export { TwoFactorAuth, twoFactorAuth } from './2fa';
